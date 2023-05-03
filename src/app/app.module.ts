217c4fcb6a9cf8372aadaa82f66a7587a8ea67bb
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2023-05-02 23:36:55 -0300

    Criação do projeto e configs iniciais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlantSearchComponent } from './plant-search/plant-search.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlantSearchComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SidebarModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/plant-search/plant-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plant-search',
+  templateUrl: './plant-search.component.html',
+  styleUrls: ['./plant-search.component.scss']
+})
+export class PlantSearchComponent {
+
+}
