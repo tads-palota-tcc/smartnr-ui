0a71ac986a3c51d9d45fca8ab35b236527570c80
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2023-05-04 20:06:40 -0300

    feat:  tradução dos calendários para pt-br

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   sidebarVisible = false;
+
+  constructor(
+    private config: PrimeNGConfig, 
+    private translateService: TranslateService
+  ) {}
+
+  ngOnInit() {
+    this.translateService.setDefaultLang('pt');
+    this.translateService.get('primeng')
+      .subscribe(res => this.config.setTranslation(res));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ToastModule } from 'primeng/toast';
-
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AreasModule } from './areas/areas.module';
 import { CoreModule } from './core/core.module';
 import { PlantsModule } from './plants/plants.module';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+
+registerLocaleData(localePt);
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
@@ -27,7 +43,7 @@ import { PlantsModule } from './plants/plants.module';
     ToastModule,
     ConfirmDialogModule
   ],
-  providers: [MessageService, ConfirmationService],
+  providers: [MessageService, ConfirmationService, TranslateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
