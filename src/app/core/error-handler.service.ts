a5666336b07596730266e216e96b471ef504bb10
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2023-06-21 07:56:46 -0300

    feat: redirecionando no 403

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,4 +1,6 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
 
 @Injectable({
@@ -6,13 +8,18 @@ import { MessageService } from 'primeng/api';
 })
 export class ErrorHandlerService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService, private router: Router) { }
 
   handle(errorResponse: any) {
+    console.log(typeof errorResponse)
     let message: string;
 
     if (typeof errorResponse === 'string') {
       message = errorResponse;
+    } else if (errorResponse instanceof HttpErrorResponse && errorResponse.status === 403) {
+      message = errorResponse.error.userMessage
+      this.messageService.add({severity: 'error', detail: message});
+      this.router.navigate(['dashboard']);
     } else {
       message = errorResponse.error.userMessage || 'Erro ao processar serviço remonto. Tente novamente mais tarde.';
       console.log('Ocorreu um erro', errorResponse);
